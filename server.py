b38edbdffe20344699b0c8955aafb727ed823ac6
naddi <damiano@ippoagri.it>
2020-05-18 11:15:57 +0200

    creato server python per appendere i dati nel csv

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request ,redirect, make_response, send_from_directory
+import json
+import os
+app = Flask(__name__)
+
+
+@app.route('/regione/<path:path>')
+def server(path):
+    return send_from_directory("regioni/",path)
+    
+
+@app.route('/submit/<path:path>',methods=["POST"])
+def append_post_tofile(path):
+    morti=request.values["morti"]
+    infetti=request.values["infetti"]
+    guariti=request.values["guariti"]
+    f=open("regioni/"+path+"/dati_covid19.csv","a")
+    f.write(morti+","+infetti+","+guariti+"\n")
+    return "LA richiesta è stata memorizzata"
+    
+
+
+
+if __name__ == '__main__':
+    app.run()
